547b714b82d3da0fc5091a69f8a55abb21f248a6
Jesse Hu <huyouare@gmail.com>
2014-09-13 18:36:06 -0400

    Copy existing code from ParseChat

--- DukeStudy/AppDelegate.m
+++ DukeStudy/AppDelegate.m
@@ -6,13 +6,44 @@
 //  Copyright (c) 2014 Jesse Hu. All rights reserved.
 //
 
+#import <Parse/Parse.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
+
+#import "AppConstant.h"
+
 #import "AppDelegate.h"
+#import "GroupView.h"
+#import "PrivateView.h"
+#import "ProfileView.h"
+#import "NavigationController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [Parse setApplicationId:@"SgbZOZ1OPs3pSiC3H9vDZ9ZqF1NFYDQ75jgWAkWK"
+                  clientKey:@"jg9PNPhgqaMNpb1x683mGCERM4lJ1QX5SAIKk2Zs"];
+    
+    [PFFacebookUtils initializeFacebook];
+    
+    [PFImageView class];
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    NavigationController *nc1 = [[NavigationController alloc] initWithRootViewController:[[GroupView alloc] init]];
+	NavigationController *nc2 = [[NavigationController alloc] initWithRootViewController:[[PrivateView alloc] init]];
+	NavigationController *nc3 = [[NavigationController alloc] initWithRootViewController:[[ProfileView alloc] init]];
+    
+	self.tabBarController = [[UITabBarController alloc] init];
+	self.tabBarController.viewControllers = [NSArray arrayWithObjects:nc1, nc2, nc3, nil];
+	self.tabBarController.tabBar.barTintColor = COLOR_TABBAR_BACKGROUND;
+	self.tabBarController.tabBar.tintColor = COLOR_TABBAR_LABEL;
+	self.tabBarController.tabBar.translucent = NO;
+	self.tabBarController.selectedIndex = 0;
+    
+	self.window.rootViewController = self.tabBarController;
+	[self.window makeKeyAndVisible];
+    
     return YES;
 }
 							
@@ -36,11 +67,19 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - Facebook responses
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+	return [FBAppCall handleOpenURL:url sourceApplication:sourceApplication withSession:[PFFacebookUtils session]];
 }
 
 @end
