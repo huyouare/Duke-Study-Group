fdcf4ccbf243a0a674bfc979313d1fba891b06a0
Jesse Hu <huyouare@gmail.com>
2014-09-13 19:22:17 -0400

    Edit photo, start adding view for new study group

---
+++ Classes/Tabs/GroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupViewController.h
+//  DukeStudy
+//
+//  Created by Jesse Hu on 9/13/14.
+//  Copyright (c) 2014 Jesse Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupViewController : UIViewController
+
+@end
