4ccb544dc35cc06c9c6d166c0a284da0f9c1da32
Jesse Hu <huyouare@gmail.com>
2014-09-13 17:41:58 -0400

    Initial Commit

---
+++ DukeStudy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DukeStudy
+//
+//  Created by Jesse Hu on 9/13/14.
+//  Copyright (c) 2014 Jesse Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DukeStudy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DukeStudy
+//
+//  Created by Jesse Hu on 9/13/14.
+//  Copyright (c) 2014 Jesse Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
