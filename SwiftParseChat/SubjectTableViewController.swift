17ffa2e919e699fcf419c38197f196580d2ae3dd
Jesse Hu <huyouare@gmail.com>
2015-03-09 02:58:47 -0400

    Add Python script for scraping courses

---
+++ SwiftParseChat/SubjectTableViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SubjectTableViewController.swift
+//  SwiftParseChat
+//
+//  Created by Jesse Hu on 3/9/15.
+//  Copyright (c) 2015 Jesse Hu. All rights reserved.
+//
+
+import UIKit
+
+class SubjectTableViewController: UITableViewController {
+
+    var subjects: NSArray!
+    var courses: NSArray!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let path = NSBundle.mainBundle().pathForResource("courses", ofType: "json") {
+            if let jsonData = NSData.dataWithContentsOfMappedFile(path) as? NSData {
+                var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+                self.subjects = jsonResult.objectForKey("subject") as NSArray!
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.subjects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+
+        if let subject = self.subjects[indexPath.row] as? NSDictionary {
+            if let subjectCode = subject["code"] as? String {
+                cell.textLabel?.text = subjectCode
+            }
+            if let subjectDesc = subject["desc"] as? String {
+                cell.detailTextLabel?.text = subjectDesc
+            }
+        }
+
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let subject = self.subjects[indexPath.row] as? NSDictionary {
+            if let courses = subject["courses"] as? NSArray {
+                self.courses = courses
+                self.performSegueWithIdentifier("subjectToCourseSegue", sender: self)
+            }
+        }
+    }
+
+    // MARK: - Navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "subjectToCourseSegue" {
+            let courseVC = segue.destinationViewController as CourseTableViewController
+            courseVC.courses = self.courses
+        }
+    }
+
+}
