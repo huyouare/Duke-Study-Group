c121a34f0c3318f0c194185f72d15221e7e072f1
Jesse Hu <huyouare@gmail.com>
2015-03-21 04:30:04 -0400

    Implement duke auth via FB Educations

--- SwiftParseChat/WelcomeViewController.swift
+++ SwiftParseChat/WelcomeViewController.swift
@@ -16,6 +16,18 @@ class WelcomeViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,7 +35,7 @@ class WelcomeViewController: UIViewController {
     
     @IBAction func facebookLogin(sender: UIButton) {
         ProgressHUD.show("Signing in...", interaction: false)
-        PFFacebookUtils.logInWithPermissions(["public_profile", "email", "user_friends"], block: { (user: PFUser!, error: NSError!) -> Void in
+        PFFacebookUtils.logInWithPermissions(["public_profile", "email", "user_friends", "user_education_history"], block: { (user: PFUser!, error: NSError!) -> Void in
             if user != nil {
                 if user[PF_USER_FACEBOOKID] == nil {
                     self.requestFacebook(user)
@@ -42,12 +54,35 @@ class WelcomeViewController: UIViewController {
         })
     }
     
+    func hasDukeEducation(userData: [String: AnyObject]!) -> Bool {
+        println(userData["education"] as NSArray)
+        for object in userData["education"] as NSArray {
+            let dict = object as NSDictionary
+            println(dict["school"])
+            if let school = dict["school"] as? NSDictionary {
+                println(school["name"])
+                if let schoolName = school["name"] as? String {
+                    if schoolName.lowercaseString.rangeOfString("duke") != nil {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
     func requestFacebook(user: PFUser) {
         var request = FBRequest.requestForMe()
         request.startWithCompletionHandler { (connection: FBRequestConnection!, result: AnyObject!, error: NSError!) -> Void in
             if error == nil {
                 var userData = result as [String: AnyObject]!
-                self.processFacebook(user, userData: userData)
+                if (self.hasDukeEducation(userData)) {
+                    self.processFacebook(user, userData: userData)
+                }
+                else {
+                    PFUser.logOut()
+                    ProgressHUD.showError("Please authenticate with Duke NetID")
+                }
             } else {
                 PFUser.logOut()
                 ProgressHUD.showError("Failed to fetch Facebook user data")
