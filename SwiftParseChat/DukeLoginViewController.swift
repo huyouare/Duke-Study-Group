19afcad4e89dbfc28f697b983c1d4ba113318e84
Jesse Hu <huyouare@gmail.com>
2015-03-21 04:30:14 -0400

    Add Duke Login to WelcomeVC

---
+++ SwiftParseChat/DukeLoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  DukeLoginViewController.swift
+//  SwiftParseChat
+//
+//  Created by Jesse Hu on 3/21/15.
+//  Copyright (c) 2015 Jesse Hu. All rights reserved.
+//
+
+import UIKit
+
+class DukeLoginViewController: UITableViewController {
+
+    @IBOutlet var netIdField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "dismissKeyboard"))
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.netIdField.becomeFirstResponder()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+
+    @IBAction func loginButtonPressed(sender: UIButton) {
+        let netId = netIdField.text.lowercaseString
+        let password = passwordField.text
+        
+        if countElements(netId) == 0 {
+            ProgressHUD.showError("NetID field is empty.")
+            return
+        } else {
+            ProgressHUD.showError("Password field is empty.")
+        }
+        
+        ProgressHUD.show("Signing in...", interaction: true)
+        
+//        PFUser.logInWithUsernameInBackground(email, password: password) { (user: PFUser!, error: NSError!) -> Void in
+//            if user != nil {
+//                PushNotication.parsePushUserAssign()
+//                ProgressHUD.showSuccess("Welcome back, \(user[PF_USER_FULLNAME])!")
+//                self.dismissViewControllerAnimated(true, completion: nil)
+//            } else {
+//                if let info = error.userInfo {
+//                    ProgressHUD.showError(info["error"] as String)
+//                }
+//            }
+//        }
+        
+    }
+}
